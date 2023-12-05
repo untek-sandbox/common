b524ffdb16240fc0c6fcb6fd7b398e2158454e2b
vitaliy <theyamshikov@yandex.ru>
2023-12-05 12:37:21 +0600

    upd

---
+++ src/Database/Memory/Abstract/AbstractFileCrudRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Untek\Database\Memory\Abstract;
+
+use Untek\Component\FormatAdapter\StoreFile;
+
+abstract class AbstractFileCrudRepository extends AbstractMemoryCrudRepository
+{
+
+    public function __construct(protected string $fileName)
+    {
+    }
+
+    protected function loadCollection(): void {
+        if(empty($this->collection)) {
+            $items = $this->getStoreFile()->load();
+            if($items) {
+                foreach ($items as $item) {
+                    $this->collection[] = $this->restoreEntity($item);
+                }
+            }
+        }
+    }
+
+    protected function dumpCollection(): void {
+        $itemsRaw = [];
+        foreach ($this->collection as $entity) {
+            $itemsRaw[] = $this->serializeEntity($entity);
+        }
+        $this->getStoreFile()->save($itemsRaw);
+    }
+
+    protected function getStoreFile(): StoreFile {
+        return new StoreFile($this->fileName);
+    }
+}
