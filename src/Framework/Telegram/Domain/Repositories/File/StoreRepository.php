8c96222838c9bdc3eae82df10cd0e30eb88f122f
vitaliy <theyamshikov@yandex.ru>
2023-12-10 22:23:41 +0600

    upd

---
+++ src/Framework/Telegram/Domain/Repositories/File/StoreRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Untek\Framework\Telegram\Domain\Repositories\File;
+
+
+use Untek\Component\FormatAdapter\StoreFile;
+
+class StoreRepository
+{
+
+    public function getLastId()
+    {
+        $storeInstance = $this->getStoreInstance();
+        $storeData = $storeInstance->load();
+        $lastId = $storeData['last_update_id'] ?? null;
+        return $lastId;
+    }
+
+    public function setLastId($lastId)
+    {
+        $storeInstance = $this->getStoreInstance();
+        $storeData = $storeInstance->load();
+        $storeData['last_update_id'] = $lastId;
+        $storeInstance->save($storeData);
+    }
+    
+    private function getStoreInstance(): StoreFile {
+        $stateFile = __DIR__ . '/../../../../../../../var/state.json';
+        $storeFile = new StoreFile($stateFile);
+        return $storeFile;
+    }
+}
