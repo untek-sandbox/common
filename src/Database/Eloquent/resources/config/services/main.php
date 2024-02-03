44e37eda753a3fbd0d3cabcc497474fe2e71188f
vitaliy <theyamshikov@yandex.ru>
2024-02-03 19:36:26 +0600

    upd

--- src/Database/Eloquent/resources/config/services/main.php
+++ src/Database/Eloquent/resources/config/services/main.php
@@ -1,20 +1,20 @@
 <?php
 
-use Untek\Database\Eloquent\Domain\Capsule\Manager;
-use Untek\Database\Eloquent\Domain\Factories\ManagerFactory;
 use Illuminate\Database\Capsule\Manager as CapsuleManager;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Untek\Database\Eloquent\Domain\Capsule\Manager;
+use Untek\Database\Eloquent\Domain\Factories\ManagerFactory;
+use Untek\Database\Eloquent\Domain\Orm\EloquentOrm;
+use Untek\Model\EntityManager\Interfaces\TransactionInterface;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $configurator): void {
     $services = $configurator->services();
 
-/*[
-    Manager::class => function () {
-        return ManagerFactory::createManagerFromEnv();
-    },
-    CapsuleManager::class => Manager::class,
-];*/
+    $services->set(TransactionInterface::class, EloquentOrm::class)
+        ->args([
+            service(Manager::class),
+        ]);
 
     $services->set(Manager::class, Manager::class)
         ->factory([ManagerFactory::class, 'createManagerFromEnv']);
