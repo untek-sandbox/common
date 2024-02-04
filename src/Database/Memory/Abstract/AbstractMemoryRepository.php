626f5b8f5232bc3a54024ff743f3ea3d74939826
vitaliy <theyamshikov@yandex.ru>
2024-02-04 18:55:42 +0600

    upd

--- src/Database/Memory/Abstract/AbstractMemoryRepository.php
+++ src/Database/Memory/Abstract/AbstractMemoryRepository.php
@@ -7,37 +7,15 @@ use Doctrine\Common\Collections\Expr\Comparison;
 use Doctrine\Persistence\ObjectRepository;
 use Untek\Component\Relation\Traits\RepositoryRelationTrait;
 use Untek\Core\Collection\Libs\Collection;
-use Untek\Database\Base\Hydrator\DefaultHydrator;
-use Untek\Database\Base\Hydrator\HydratorInterface;
+use Untek\Database\Base\Hydrator\Traits\HydratorTrait;
 
 abstract class AbstractMemoryRepository implements ObjectRepository
 {
 
     use RepositoryRelationTrait;
-
-    protected HydratorInterface $mapper;
+    use HydratorTrait;
 
     abstract protected function getItems(): array;
-
-    protected function getHydrator(): HydratorInterface
-    {
-        if (isset($this->mapper)) {
-            return $this->mapper;
-        }
-        return new DefaultHydrator($this->getClassName());
-    }
-
-    protected function dehydrate(object $entity): array
-    {
-        $data = $this->getHydrator()->dehydrate($entity);
-        return $data;
-    }
-
-    protected function hydrate(array $item): object
-    {
-        $entity = $this->getHydrator()->hydrate($item);
-        return $entity;
-    }
 
     public function find($id, ?array $relations = null)
     {
