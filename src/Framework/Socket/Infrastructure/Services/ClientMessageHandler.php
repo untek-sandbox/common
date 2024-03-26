8f535a3931c832fbf9b2247e1628e34f53424e29
Vitaliy <theyamshikov@yandex.ru>
2024-03-26 18:21:30 +0500

    upd

--- src/Framework/Socket/Infrastructure/Services/ClientMessageHandler.php
+++ src/Framework/Socket/Infrastructure/Services/ClientMessageHandler.php
@@ -3,7 +3,10 @@
 
 namespace Untek\Framework\Socket\Infrastructure\Services;
 
+use Untek\Core\Code\Helpers\DeprecateHelper;
 use Untek\Framework\Socket\Domain\Interfaces\Services\ClientMessageHandlerInterface;
+
+DeprecateHelper::hardThrow();
 
 class ClientMessageHandler implements ClientMessageHandlerInterface
 {
