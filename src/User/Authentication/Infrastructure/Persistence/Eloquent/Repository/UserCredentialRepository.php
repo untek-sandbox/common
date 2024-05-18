ae14c474145db4d50cf06189cec0124aec1e6f97
Vitaliy <theyamshikov@yandex.ru>
2024-05-18 23:18:16 +0500

    upd

--- src/User/Authentication/Infrastructure/Persistence/Eloquent/Repository/UserCredentialRepository.php
+++ src/User/Authentication/Infrastructure/Persistence/Eloquent/Repository/UserCredentialRepository.php
@@ -47,12 +47,12 @@ class UserCredentialRepository extends AbstractEloquentCrudRepository implements
     protected function hydrate(array $item): object
     {
         $item['identity_id'] = $item['user_id'];
-        return parent::hydrate($item);
+        return parent::denormalize($item);
     }
 
     protected function dehydrate(object $entity): array
     {
-        $item = parent::dehydrate($entity);
+        $item = parent::normalize($entity);
         $item['user_id'] = $item['identity_id'];
         unset($item['identity_id']);
         $item['created_at'] = (new \DateTimeImmutable())->format(\DateTimeImmutable::ISO8601);
