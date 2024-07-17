3e1e6d57e617cd77074b157ea48ffa00c2198f3a
Vitaliy <theyamshikov@yandex.ru>
2024-07-17 20:50:27 +0500

    upd

--- src/Component/LogReader/resources/config/services/site.php
+++ src/Component/LogReader/resources/config/services/site.php
@@ -1,39 +1,23 @@
 <?php
 
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Untek\Component\LogReader\Presentation\Http\Site\Controllers\GetLogByIdController;
 use Untek\Component\LogReader\Presentation\Http\Site\Controllers\LogListController;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Untek\Component\App\Services\ControllerAccessChecker;
-use Untek\Component\Web\TwBootstrap\Widgets\Breadcrumb\BreadcrumbWidget;
-use Untek\Component\Web\View\Libs\View;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $configurator): void {
-    $services = $configurator->services()->defaults()->public();
+    $services = $configurator->services()->defaults()->public()->autowire();
 
     $services->set(LogListController::class, LogListController::class)
-        ->args([
-            service(View::class),
-            service(BreadcrumbWidget::class),
-            service(ControllerAccessChecker::class),
-        ])
         ->tag('http.controller', [
             'name' => 'log-list',
             'path' => '/log',
             'methods' => ['GET'],
-        ])
-    ;
+        ]);
 
     $services->set(GetLogByIdController::class, GetLogByIdController::class)
-        ->args([
-            service(View::class),
-            service(BreadcrumbWidget::class),
-            service(ControllerAccessChecker::class),
-        ])
         ->tag('http.controller', [
             'name' => 'log-details',
             'path' => '/log/{date}/{id}',
             'methods' => ['GET'],
-        ])
-    ;
+        ]);
 };
