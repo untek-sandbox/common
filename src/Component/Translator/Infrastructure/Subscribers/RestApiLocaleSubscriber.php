096a51e6e8908d25bf4a578a08b412c6267291ad
Vitaliy <theyamshikov@yandex.ru>
2024-08-10 12:33:32 +0500

    upd

--- src/Component/Translator/Infrastructure/Subscribers/RestApiLocaleSubscriber.php
+++ src/Component/Translator/Infrastructure/Subscribers/RestApiLocaleSubscriber.php
@@ -7,6 +7,9 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Untek\Core\Code\Helpers\DeprecateHelper;
+
+DeprecateHelper::hardThrow();
 
 class RestApiLocaleSubscriber implements EventSubscriberInterface
 {
