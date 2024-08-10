55ce3e35eaa77496a2058f321e506347ea03611c
Vitaliy <theyamshikov@yandex.ru>
2024-08-10 22:20:24 +0500

    upd

--- src/Component/App/resources/config/services/main.php
+++ src/Component/App/resources/config/services/main.php
@@ -2,9 +2,6 @@
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Untek\Component\App\Services\ControllerAccessChecker;
-use Psr\Container\ContainerInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $configurator): void {
     $services = $configurator->services()->defaults()->public()->autowire();
