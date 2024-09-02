91d116cd8c650270387d296f3e05098f95f60c3a
Vitaliy <theyamshikov@yandex.ru>
2024-09-02 18:29:47 +0500

    upd

--- src/Persistence/Normalizer/DatabaseItemNormalizer.php
+++ src/Persistence/Normalizer/DatabaseItemNormalizer.php
@@ -3,17 +3,23 @@
 namespace Untek\Persistence\Normalizer;
 
 use DateTime;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\Mapping\Loader\AttributeLoader;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
+use Symfony\Component\Serializer\Normalizer\UidNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
+use Untek\Component\Collection\CollectionNormalizer;
+use Untek\Component\ValueObject\ValueObjectNormalizer;
 use function Symfony\Component\String\u;
 
 class DatabaseItemNormalizer extends AbstarctNormalizer implements DbNormalizerInterface
@@ -21,11 +27,34 @@ class DatabaseItemNormalizer extends AbstarctNormalizer implements DbNormalizerI
 
     protected function getSerializer(): NormalizerInterface|DenormalizerInterface
     {
+        $defaultContext = [
+            /*AbstractNormalizer::IGNORED_ATTRIBUTES => [
+                'id'
+            ],
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER =>
+                function ($articles, $format, $context)  {
+                    return $articles->getId();
+                }*/
+        ];
         $classMetadataFactory = new ClassMetadataFactory(new AttributeLoader());
+        $propertyTypeExtractor = new ReflectionExtractor();
+        $objectNormalizer = new ObjectNormalizer(
+            classMetadataFactory: $classMetadataFactory,
+//            propertyTypeExtractor: $propertyTypeExtractor,
+            nameConverter: new CamelCaseToSnakeCaseNameConverter(),
+            defaultContext: $defaultContext,
+        );
         $normalizers = [
+//            new PropertyNormalizer(),
             new DateTimeNormalizer(),
+            new ValueObjectNormalizer(),
+            new CollectionNormalizer(),
+            new BackedEnumNormalizer(),
+            new UidNormalizer([
+                UidNormalizer::NORMALIZATION_FORMAT_KEY => UidNormalizer::NORMALIZATION_FORMAT_BASE58,
+            ]),
             new ArrayDenormalizer(),
-            new ObjectNormalizer($classMetadataFactory, new CamelCaseToSnakeCaseNameConverter()),
+            $objectNormalizer,
         ];
         return new Serializer($normalizers);
     }
